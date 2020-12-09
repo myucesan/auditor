e5bdfbcc92fdd855d93923fa2554d29d2c557407
Mustafa Yücesan <my96@live.nl>
2020-12-09 18:49:19 +0100

    rewriting spreadsheet code, pushing so I have backup

--- commands/price-check.js
+++ commands/price-check.js
@@ -1,9 +1,10 @@
+const ss = require("../spreadsheet");
+const fs = require('fs');
 module.exports = {
     name: 'price-check',
     description: 'Shows the price of a ship.',
     execute(message, args) {
-        const ss = require("spreadsheet");
-        const fs = require('fs');
+
         message.channel.send('**Not been implemented yet. Ask corp ship prices soon here**\n\n');
         if (args.length != 1) {
             message.channel.send(`Incorrect usage: format:\n!price-check [ship] \nEx:!price-check Vigilant`);
@@ -12,8 +13,13 @@ module.exports = {
             fs.readFile('credentials.json', (err, content) => {
                 if (err) return console.log('Error loading client secret file:', err);
 // Authorize a client with credentials, then call the Google Sheets API.
+//                 ss.authorize(JSON.parse(content), ss.lol);
                 ss.authorize(JSON.parse(content), ss.lol);
             });
+//             const auth = ss.readCredentials();
+//             ss.lol(auth, args[0]);
         }
+
+
     },
 };
--- spreadsheet.js
+++ spreadsheet.js
@@ -13,27 +13,27 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), module.exports.lol);
+    module.exports.authorize(JSON.parse(content), module.exports.lol);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
-}
+// /**
+//  * Create an OAuth2 client with the given credentials, and then execute the
+//  * given callback function.
+//  * @param {Object} credentials The authorization client credentials.
+//  * @param {function} callback The callback to call with the authorized client.
+//  */
+// function authorize(credentials, callback) {
+//     const {client_secret, client_id, redirect_uris} = credentials.installed;
+//     const oAuth2Client = new google.auth.OAuth2(
+//         client_id, client_secret, redirect_uris[0]);
+//
+//     // Check if we have previously stored a token.
+//     fs.readFile(TOKEN_PATH, (err, token) => {
+//         if (err) return getNewToken(oAuth2Client, callback);
+//         oAuth2Client.setCredentials(JSON.parse(token));
+//         callback(oAuth2Client);
+//     });
+// }
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -156,10 +156,11 @@ module.exports = {
             });
         } else {
             console.log("Ship is null");
+            return null;
         }
 
     },
-    authorize: function(credentials, callback) {
+    authorize(credentials, callback) {
         const {client_secret, client_id, redirect_uris} = credentials.installed;
         const oAuth2Client = new google.auth.OAuth2(
             client_id, client_secret, redirect_uris[0]);
@@ -169,6 +170,14 @@ module.exports = {
             if (err) return getNewToken(oAuth2Client, callback);
             oAuth2Client.setCredentials(JSON.parse(token));
             callback(oAuth2Client);
+        });
+    },
+    readCredentials(shipName) {
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Sheets API.
+            module.exports.authorize(JSON.parse(content), module.exports.lol);
+
         });
     }
 }
