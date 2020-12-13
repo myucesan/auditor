e8d09fff0dd5893b0c9d9f218bd838ad34297d84
Mustafa Yücesan <my96@live.nl>
2020-12-13 23:14:06 +0100

    reformat !!stock with fixed date

--- commands/stock.js
+++ commands/stock.js
@@ -1,6 +1,7 @@
 const ss = require("../spreadsheet");
 const credentials = require('../credentials.json');
 const token = require('../token.json');
+const Discord = require("discord.js");
 const {google} = require('googleapis');
 
 module.exports = {
@@ -18,6 +19,19 @@ module.exports = {
             expiry_date: token.expiry_date
         };
 
+        const mineralStock = new Discord.MessageEmbed()
+            .setColor('#FFFFFF')
+            .setTitle('EVO1 Mineral Stock');
+
+        const mineralRequired = new Discord.MessageEmbed()
+            .setColor('90f542')
+            .setTitle('EVO2 Mineral Required')
+
+
+
+
+
+
         const oAuth2Client = new google.auth.OAuth2(
             client_id, client_secret, redirect_uris[0]);
         oAuth2Client.setCredentials(creds);
@@ -28,10 +42,16 @@ module.exports = {
                 rows.map((row => {
                     // console.log("----------------");
                     row.values.forEach(player => {
-                        message.channel.send(`**Mineral: ${player[0]}** | Total Stock: ${player[1]} | Required Amount: ${player[2]}\n`);
+                        // message.channel.send(`**Mineral: ${player[0]}** | Total Stock: ${player[1]} | Required Amount: ${player[2]}\n`);
+                        mineralStock.addField(player[0], player[1], true);
+                        mineralRequired.addField(player[0], player[2], true);
                     });
                 }))
+                mineralStock.addField("Last updated:", "01-01-2021 (static)", true);
+                mineralRequired.addField("Last updated:", "01-01-2021 (static)", true);
             }
+            message.channel.send(mineralStock);
+            message.channel.send(mineralRequired);
 
         }).catch(error => {
             console.log(error);
